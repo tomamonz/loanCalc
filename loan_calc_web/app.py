b18865828a811fc3db973210ad5acf296c622ec3
Tom Zal <Tomasz.Zaleski@fdmgroup.com>
2025-10-07 16:11:49 +0200

    app.py simplyfing

--- loan_calc_web/app.py
+++ loan_calc_web/app.py
@@ -169,7 +169,4 @@ def clear_comparisons():
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HOST", "0.0.0.0")
-    port = int(os.environ.get("PORT", "8710"))
-    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
-    app.run(host=host, port=port, debug=debug)
+    app.run(host="0.0.0.0", port=8710, debug=True)
